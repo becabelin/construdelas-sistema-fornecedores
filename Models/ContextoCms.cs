b94f8c0bdcb8749b15fd86fefee4693b7bb170dd
Rebeca Belin <becabelin@gmail.com>
2021-11-07 12:52:27 -0300

    puxando para o github

---
+++ Models/ContextoCms.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Completo.Models
+{
+  public class ContextoCms : DbContext
+  {
+    public ContextoCms(DbContextOptions<ContextoCms> options) : base(options) { }
+
+    public ContextoCms() { }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+  }
+}
